2a90dfad5211ba481c05b8fe02ec386058450766
海一长虹项目 <tuotuoyi@haiyichangxingnantongdeMac-mini.local>
2016-01-30 21:43:23 +0800

    创建了Person类

---
+++ MeCache/MeCache/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  MeCache
+//
+//  Created by 海一长虹项目 on 16/1/30.
+//  Copyright (c) 2016年 com.cvicse.xu_zyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
